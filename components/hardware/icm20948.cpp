54be5afbd0375c0aea632d1b66280ee7f767ba62
Ender-robot <2120299823@qq.com>
2025-11-28 22:17:01 +0800

    ICM20948驱动初始化操作完成

--- components/hardware/icm20948.cpp
+++ components/hardware/icm20948.cpp
@@ -10,9 +10,21 @@ ICM20948::~ICM20948() {
 }
 
 /**
+ * @brief 切换寄存器组
+ * 
+ * @param bankNum 寄存器组序号
+ */
+bool ICM20948::selUserBank(uint8_t bankNum) {
+    if (!(bankNum == 0x00 || bankNum == 0x10 || bankNum == 0x20 || bankNum == 0x30)) return false;
+    bool ret = i2c.write_byte_to_mem(ICM20948_ADDR, REG_BANK_SEL, bankNum);
+    return ret;
+}
+
+/**
  * @brief 唤醒传感器
  */
 bool ICM20948::wakeUp() {
+    if (!selUserBank(USER_BANK_0)) return false;
     bool ret = i2c.write_byte_to_mem(ICM20948_ADDR, PWR_MGMT_1, 0x01);
     return ret;
 }
@@ -25,4 +37,69 @@ bool ICM20948::connective() {
     i2c.read_bytes_from_mem(ICM20948_ADDR, WHO_AM_I, &data, 1);
     if (data == 0xEA) return true;
     else return false;
+}
+
+/**
+ * @brief 初始化设置ICM
+ * 
+ * @param PWR_MGMT_2_PARAM 唤醒陀螺仪和加速度计，默认启用所有轴
+ * @param GYRO_CONFIG_1_PARAM 设置陀螺仪，默认失能低通滤波器，+-500°/s量程
+ * @param ACCEL_CONFIG_PARAM 设置加速度计，默认失能低通滤波器，+-4g量程
+ * @param I2C_SLV4_DO_PARAM 给从机4的控制命令，默认是磁力计，100Hz连续测量模式
+ * 
+ * @note 初始化很多参数都是硬编码，可在初始化完后再单独设置
+ */
+bool ICM20948::init(
+    uint8_t PWR_MGMT_2_PARAM,
+    uint8_t GYRO_CONFIG_1_PARAM,
+    uint8_t ACCEL_CONFIG_PARAM,
+    uint8_t I2C_SLV4_DO_PARAM
+)
+{
+    //--- 唤醒并进行连接性检查 ---//
+    if (!wakeUp()) return false;
+    if (!connective()) return false;
+
+    //--- 使能外设电源 ---//
+    selUserBank(USER_BANK_0);
+    i2c.write_byte_to_mem(ICM20948_ADDR, PWR_MGMT_2, PWR_MGMT_2_PARAM);
+
+    //--- 设置陀螺仪 ---//
+    selUserBank(USER_BANK_2);
+    i2c.write_byte_to_mem(ICM20948_ADDR, GYRO_CONFIG_1, GYRO_CONFIG_1_PARAM); // 默认失能低通滤波器，500°量程
+
+    //--- 设置加速度计 ---//
+    i2c.write_byte_to_mem(ICM20948_ADDR, ACCEL_CONFIG, ACCEL_CONFIG_PARAM); // 默认失能低通滤波器，+-4g
+
+    //--- 设置磁力计 ---//
+    /*磁力计实际是独立与ICM的一颗芯片，可选择直接挂载总线上，或者把ICM当主机，从ICM读取AK磁力计的数据*/
+    selUserBank(USER_BANK_0);
+
+    // 默认禁用旁路模式通过ICM与AK通讯
+    uint8_t INT_PIN_CFG_DATA;
+    i2c.read_bytes_from_mem(ICM20948_ADDR, INT_PIN_CFG, &INT_PIN_CFG_DATA, 1);
+    INT_PIN_CFG_DATA &= ~0x02; // 确保禁用旁路模式
+    i2c.write_byte_to_mem(ICM20948_ADDR, INT_PIN_CFG, INT_PIN_CFG_DATA);
+
+    // 默认启用主机模式
+    uint8_t USER_CTRL_DATA;
+    i2c.read_bytes_from_mem(ICM20948_ADDR, USER_CTRL, &USER_CTRL_DATA, 1);
+    USER_CTRL_DATA |= 0x20; // 启用主机模式
+    i2c.write_byte_to_mem(ICM20948_ADDR, USER_CTRL, USER_CTRL_DATA);
+    // 设置主机模式参数
+    selUserBank(USER_BANK_3);
+    i2c.write_byte_to_mem(ICM20948_ADDR, I2C_MST_CTRL, 0x07); // IIC 时钟频率，默认400kHz（推荐）
+    // 配置磁力计
+    i2c.write_byte_to_mem(ICM20948_ADDR, I2C_SLV4_ADDR, AK09916_I2C_ADDR); // 指定磁力计地址
+    i2c.write_byte_to_mem(ICM20948_ADDR, I2C_SLV4_REG, AK09916_CNTL2); // 指定磁力计的控制寄存器地址
+    i2c.write_byte_to_mem(ICM20948_ADDR, I2C_SLV4_DO, I2C_SLV4_DO_PARAM); // 默认连续测量模式100Hz
+    i2c.write_byte_to_mem(ICM20948_ADDR, I2C_SLV4_CTRL, 0x80); // 启用传输，执行我们设置的以上操作
+    // 为IIC主机设置从何读取磁力计数据
+    i2c.write_byte_to_mem(ICM20948_ADDR, I2C_SLV0_ADDR, AK09916_I2C_ADDR | 0x80); // AK09916 的 I2C 地址 + 读标志位 (bit 7 = 1) -> 0x0C | 0x80 = 0x8C
+    i2c.write_byte_to_mem(ICM20948_ADDR, I2C_SLV0_REG, AK09916_HXL); // 设置IIC主机要读取的磁力计数据起始地址
+    i2c.write_byte_to_mem(ICM20948_ADDR, I2C_SLV0_CTRL, 0x89); // 起始位向后读取9个B
+
+    selUserBank(USER_BANK_0);
+
+    return true;
 }
--- components/hardware/icm20948.hpp
+++ components/hardware/icm20948.hpp
@@ -20,17 +20,55 @@ class ICM20948 {
         ICM20948(I2C& driver);
         ~ICM20948();
 
+        bool selUserBank(uint8_t bankNum);
         bool wakeUp();
         bool connective();
+        bool init(
+            uint8_t PWR_MGMT_2_PARAM = 0x00,
+            uint8_t GYRO_CONFIG_1_PARAM = 0x03,
+            uint8_t ACCEL_CONFIG_PARAM = 0x03,
+            uint8_t I2C_SLV4_DO_PARAM = 0x08
+        );
+
+        bool readGyro(Vec3f& data); // 读陀螺仪
 
     private:
         static constexpr const char* TAG = "ICM20948"; // 日志标签
         
-        enum REG : uint8_t {
-            ICM20948_ADDR = 0x68, // 设备地址
-            PWR_MGMT_1    = 0x06, // 设备电源及时钟管理寄存器
-            WHO_AM_I      = 0x00, // 设备识别寄存器
-            REG_BANK_SEL  = 0x7F, // 组选择寄存器
+        enum PARAMS : uint8_t {
+            ICM20948_ADDR        = 0x68, // ICM设备地址
+            AK09916_I2C_ADDR     = 0x0C, // AK设备地址
+
+            // AK
+            AK09916_CNTL2        = 0x31, // 磁力计的控制寄存器
+            AK09916_HXL          = 0x11, // 磁力计数据寄存器的起始地址
+
+            // User Bank
+            USER_BANK_0          = 0x00, // 零号寄存器组
+            USER_BANK_1          = 0x10, // 一号寄存器组
+            USER_BANK_2          = 0x20, // 二号寄存器组
+            USER_BANK_3          = 0x30, // 三号寄存器组
+
+            // Bank 0
+            REG_BANK_SEL         = 0x7F, // 组选择寄存器
+            PWR_MGMT_1           = 0x06, // 主电源及时钟管理寄存器
+            PWR_MGMT_2           = 0x07, // 陀螺仪及加速度计电源管理寄存器
+            WHO_AM_I             = 0x00, // 设备识别寄存器
+            USER_CTRL            = 0x03, // 用与控制传感器的主要功能
+            INT_PIN_CFG          = 0x0F, // 中断引脚配置
+            EXT_SLV_SENS_DATA_00 = 0x3B, // 在配置好 I2C 主机读取操作后，磁力计的数据将被存放在这些寄存器中
+            // Bank 2
+            GYRO_CONFIG_1        = 0x01, // 陀螺仪配置1，用于配置滤波器和量程
+            ACCEL_CONFIG         = 0x14, // 加速度计配置1，用于配置滤波器和量程
+            // Bank 3
+            I2C_MST_CTRL         = 0x01, // IIC主机控制
+            I2C_SLV0_ADDR        = 0x03, // 设置磁力计的 I2C 地址
+            I2C_SLV0_REG         = 0x04, // 设置要读取的磁力计内部寄存器的起始地址
+            I2C_SLV0_CTRL        = 0x05, // 启用从机读取操作并设置读取的数据长度
+            I2C_SLV4_ADDR        = 0x13, // 用于指定磁力计地址
+            I2C_SLV4_REG         = 0x14, // 用于指定磁力计控制寄存器地址
+            I2C_SLV4_DO          = 0x16, // 用于为磁力计设置控制位
+            I2C_SLV4_CTRL        = 0x15, // 用于启用SLV4的所有设置
         };
 
         I2C& i2c;
